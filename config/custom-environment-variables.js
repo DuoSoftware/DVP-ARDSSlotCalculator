12898608e9a23ed3918099b49f3b022f843dc49c
Heshan Indika <k.heshan.indika@gmail.com>
2018-06-11 18:04:26 +0530

    remove unnecessary environment variables

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -11,14 +11,5 @@ module.exports = {
                 "port": "SYS_REDIS_SENTINEL_PORT",
                 "name": "SYS_REDIS_SENTINEL_NAME"
             }
-        },
-    "Host":
-        {
-            "LBIP": "LB_FRONTEND",
-            "LBPort": "LB_PORT",
-            "Port": "HOST_ARDSLITESERVICE_PORT",
-            "Version": "HOST_VERSION",
-            "UseMsgQueue": "HOST_USE_MSG_QUEUE",
-            "UseDashboardMsgQueue": 'HOST_USE_DASHBOARD_MSG_QUEUE'
         }
 };
--- config/default.js
+++ config/default.js
@@ -12,13 +12,5 @@ module.exports = {
                 "port":16389,
                 "name":"redis-cluster"
             }
-        },
-    "Host":
-        {
-            "LBIP": "127.0.0.1",
-            "LBPort": "8828",
-            "Ip": "127.0.0.1",
-            "Port": "8830",
-            "Version": "1.0.0.0",
         }
 };
