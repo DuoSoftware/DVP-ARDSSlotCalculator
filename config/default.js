8830be2beee6771474ed1a483549d280f034f33c
sine19 <sineth17@gmail.com>
2018-06-12 14:36:53 +0530

    clear default values

--- config/default.js
+++ config/default.js
@@ -2,13 +2,13 @@ module.exports = {
     "Redis":
         {
             "mode":"sentinel",//instance, cluster, sentinel
-            "ip": "45.55.142.207",
+            "ip": "",
             "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "user": "",
+            "password": "",
             "db":6,
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+                "hosts": "",
                 "port":16389,
                 "name":"redis-cluster"
             }
